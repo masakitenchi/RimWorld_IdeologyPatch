858c09ec15d3c9aa7951db22915d9c3adcba1c4e
Masakitenchi <fucong119@gmail.com>
2022-05-04 09:24:56 +0900

    Fixed not demanding treatment while have mood penalty

--- AgeReversalAgeRebalanced/AgeReversalRebalanced.cs
+++ AgeReversalAgeRebalanced/AgeReversalRebalanced.cs
@@ -5,7 +5,7 @@ using Verse;
 
 
 
-namespace IdeologyPatch
+namespace AgeReversalAgeRebalanced
 {
     [StaticConstructorOnStartup]
     public static class HarmonyPatches
@@ -16,21 +16,21 @@ namespace IdeologyPatch
         {
             Log.Message("IdeologyPatch Loaded");
             patchType = typeof(HarmonyPatches);
-            Harmony harmony= new Harmony("regex.IP");
-            harmony.Patch(AccessTools.Method(typeof(ThoughtWorker_AgeReversalDemanded), "CanHaveThought"), null, new HarmonyMethod(patchType, "CanHaveThoughtPostfix"));
+            Harmony harmony= new Harmony("regex.ARAR");
+            harmony.Patch(AccessTools.Method(typeof(ThoughtWorker_AgeReversalDemanded), "CanHaveThoughtImpl"), null, new HarmonyMethod(patchType, "CanHaveThoughtPostfix"));
         }
         public static bool CanHaveThoughtPostfix(bool result, Pawn pawn)
         {
-            /*Log.Message("PostfixCalled");
+            Log.Message("PostfixCalled");
             Harmony.DEBUG = true;
             Log.Message("prior to postfix:" + result);
             Log.Message("CurrentAgeis:" + pawn.ageTracker.AgeBiologicalYears);
             Log.Message("ExpectedLifeis:" + pawn.RaceProps.lifeExpectancy);
             Log.Message("AgeReversalLifeStage=" + AgeReversalLifeStage);
-            Log.Message("(float) pawn.ageTracker.AgeBiologicalYears /(float)  pawn.RaceProps.lifeExpectancy=" + (float)pawn.ageTracker.AgeBiologicalYears / (float)pawn.RaceProps.lifeExpectancy);*/
+            Log.Message("(float) pawn.ageTracker.AgeBiologicalYears /(float)  pawn.RaceProps.lifeExpectancy=" + (float)pawn.ageTracker.AgeBiologicalYears / (float)pawn.RaceProps.lifeExpectancy);
             if ((float) pawn.ageTracker.AgeBiologicalYears /(float)  pawn.RaceProps.lifeExpectancy < AgeReversalLifeStage)
                 return false;
-            //Log.Message("after postfix:" + result);
+            Log.Message("after postfix:" + result);
             return result;
         }
     }
